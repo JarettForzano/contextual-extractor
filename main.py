7856badbd86cbaf75a460503dd5de416793ea24c
Jarett Forzano <jpforzanojr@gmail.com>
2024-10-18 12:48:57 -0700

    another one sorted

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 def main():
-    input_pdf = "examples/transformers.pdf"
+    input_pdf = "convert/TCP_IP.pdf"
     output_dir = "photos"
     slice_height = 150
     slice_pdf_to_images(input_pdf, output_dir, slice_height)
