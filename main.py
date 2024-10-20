565c03de9b0d71d6c271f7797237efa6c3c2cfc6
Jarett Forzano <jpforzanojr@gmail.com>
2024-10-20 12:49:26 -0700

    added more to visual

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ load_dotenv()
 
 # mv photos/*.png extra_text
 def main():
-    input_pdf = "convert/Deep Learning.pdf"
+    input_pdf = "convert/DUOATTENTION.pdf"
     output_dir = "photos"
     slice_height = 150
     slice_pdf_to_images(input_pdf, output_dir, slice_height)
     
 if __name__ == "__main__":
     main()
+
+
+# 351 Visual
+# 356 Text
+
+#### Goal is 500 each right now
